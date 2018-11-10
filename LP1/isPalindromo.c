2f4e1b87181f4c72ecc8c0f8f498b540e6f24b25
Bruno <brunocozendey@gmail.com>
2018-11-10 13:07:20 -0200

    IsPalindromo add

---
+++ LP1/isPalindromo.c
@@ -0,0 +1,28 @@
+//Identifica se o string de entrada no terminal é um palíndromo. Respodendo sim ou não.
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char main(void){
+    char string[256];
+    unsigned int i,j;
+    int palindromo;
+    palindromo = 0;
+    fgets(string,255,stdin);
+    i = 0;
+    j = strlen(string)-2;
+    while (i < j){
+        if (string[i]==' '){
+            i++;
+        }
+        if (string[j]==' '){
+            j--;
+        }
+        if (tolower(string[i]) != tolower(string[j])){
+            return printf("%s","NAO\n");
+        }
+        i++;
+        j--;
+    }
+    return printf("%s","SIM\n");
+}
